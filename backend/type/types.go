30658a3bf9aec1ca8ca68a1c03321e15206b04b7
Sankhyahrick <sankhyahricks@gmail.com>
2025-10-08 21:15:26 +0530

    3rd

--- backend/type/types.go
+++ backend/type/types.go
@@ -1,6 +1,7 @@
-// Package type contains the all the types
+// Package datatype contains the all the types
 package datatype
 
+// User for storing user data
 type User struct {
 	ID        string   `bson:"_id,omitempty"`
 	Name      string   `bson:"name"`
@@ -8,4 +9,17 @@ type User struct {
 	Password  string   `bson:"password"`
 	Interests []string `bson:"interests,omitempty"`
 	Role      string   `bson:"role"`
+}
+
+// Event for storing Event data
+type Event struct {
+	ID               string   `bson:"_id,omitempty"`
+	Title            string   `bson:"title"`
+	Description      string   `bson:"description"`
+	Category         []string `bson:"Category"`
+	Location         string   `bson:"location"`
+	Price            float64  `bson:"price"`
+	TicketsAvailable int      `bson:"ticketsAvailable"`
+	OrganizerID      string   `bson:"organizerID,omitempty"`
+	ImageURL         string   `bson:"imageURL,omitempty"`
 }
